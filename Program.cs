57cd2b686906ee11755870ed0362700348ff0b52
Kiran-Pradha <kiranpradha30@gmail.com>
2025-06-18 10:34:49 +0530

    created

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@ using System;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Use 0.0.0.0 to allow external access (e.g., Docker, Render, etc.)
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://*:{port}");
+app.Urls.Add($"http://0.0.0.0:{port}");
 
 app.MapGet("/", () => "Hello from .NET 8 minimal web app!");
 
--- obj/Debug/net8.0/dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba27b782ce0d94f34483675f2c267232639203b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
